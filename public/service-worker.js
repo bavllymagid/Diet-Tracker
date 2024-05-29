a9406b16c6dee705f9c4dc5173891a11f37bc82d
Bavlly magid <pavllymagid04@gmail.com>
2024-05-29 20:37:32 +0300

    [ADDED] PWA for offline app

---
+++ public/service-worker.js
@@ -0,0 +1,42 @@
+// public/service-worker.js
+
+const CACHE_NAME = 'my-app-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/bundle.js',
+];
+
+this.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+this.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      );
+    })
+  );
+});
