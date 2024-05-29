e0c33368f0f43926fca12bfeb88d8bd3fd63cc5d
Bavlly magid <pavllymagid04@gmail.com>
2024-05-29 20:44:34 +0300

    [ADDED] PWA for offline app

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,9 +5,11 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/static/js/bundle.js',
+  // Add other assets you want to cache
 ];
 
-this.addEventListener('install', (event) => {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
@@ -16,16 +18,45 @@ this.addEventListener('install', (event) => {
   );
 });
 
-this.addEventListener('fetch', (event) => {
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        return response || fetch(event.request);
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it's a stream
+        const fetchRequest = event.request.clone();
+
+        // Try fetching the request from the network
+        return fetch(fetchRequest).then((response) => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response because it's a stream
+          const responseToCache = response.clone();
+
+          // Open the cache and cache the response
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
       })
   );
 });
 
-this.addEventListener('activate', (event) => {
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -34,9 +65,9 @@ this.addEventListener('activate', (event) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
-          return null;
         })
       );
     })
   );
 });
+
