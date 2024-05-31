c5b8816f8241d54ac27642cc4d8bc68f90fa1109
Bavlly magid <pavllymagid04@gmail.com>
2024-05-31 04:20:43 +0300

    [FIXED] offline caching

--- craco.config.js
+++ craco.config.js
@@ -2,41 +2,18 @@ const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // Add WorkboxWebpackPlugin to generate service worker
-      webpackConfig.plugins.push(
-        new GenerateSW({
-          clientsClaim: true,
-          skipWaiting: true,
-          navigateFallback: '/index.html',
-          // Define the URLs to cache (e.g., assets, pages)
-          // Adjust these URLs based on your app's structure
-          runtimeCaching: [
-            {
-              // Cache all API requests with NetworkFirst strategy
-              urlPattern: /^https:\/\/api\.example\.com/,
-              handler: 'NetworkFirst',
-              options: {
-                cacheName: 'api-cache',
-                networkTimeoutSeconds: 10,
-              },
+    plugins: [
+      new GenerateSW({
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:js,css,html)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'static-resources',
             },
-            {
-              // Cache other assets with StaleWhileRevalidate strategy
-              urlPattern: /\.(?:png|jpg|jpeg|svg|js|css)$/,
-              handler: 'CacheFirst',
-              options: {
-                cacheName: 'assets-cache',
-                expiration: {
-                  maxEntries: 100,
-                  maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-                },
-              },
-            },
-          ],
-        })
-      );
-      return webpackConfig;
-    },
+          },
+        ],
+      }),
+    ],
   },
 };
